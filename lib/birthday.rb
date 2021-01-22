06739f09ee09c5d17a3273023ea30af72119a7a6
Yu Shi <yushiys95@gmail.com>
2021-01-22 02:59:01 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,5 +5,12 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids = {
+"Timmy" => 9, 
+"Sarah" => 6, 
+"Amanda" => 27
+}
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
