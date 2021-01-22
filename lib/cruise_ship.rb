d7dc1b0e1744dc7cb09e220e5bc4272ab2f38d61
Yu Shi <yushiys95@gmail.com>
2021-01-22 02:52:01 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,6 +6,9 @@
 # suite_e: "Crumpet the Elf"
 # }
 
-def select_winner(passengers)
-  # add the code snippet here!
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
 end
